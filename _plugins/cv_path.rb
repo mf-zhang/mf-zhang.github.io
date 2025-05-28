94a4e6d942568a334da7e522da49e707ece2ca83
Your Name <you@example.com>
2025-05-28 22:57:39 +0900

    cv

--- _plugins/cv_path.rb
+++
@@ -1,9 +0,0 @@
-module Jekyll
-  class CVPathTag < Liquid::Tag
-    def render(context)
-      context.registers[:site].data['cv']['latest_cv_path']
-    end
-  end
-end
-
-Liquid::Template.register_tag('cv_path', Jekyll::CVPathTag) 
